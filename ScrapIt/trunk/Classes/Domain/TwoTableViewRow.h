d952450e62e68bcae6dc1d47ab3496bdfee51b2f
dgaudet86 <dgaudet86@9b412eb8-cd5c-11de-b49f-713b138df6af>
2012-12-13 04:56:45 +0000

    - Adding TwoTableViewRow class

---
+++ ScrapIt/trunk/Classes/Domain/TwoTableViewRow.h
@@ -0,0 +1,23 @@
+//
+//  TwoTableViewRow.h
+//  ScrapIt
+//
+//  Created by Dean Gaudet on 2012-12-12.
+//
+//
+
+#import "TableViewRow.h"
+
+@interface TwoTableViewRow : TableViewRow {
+    UIFont *_font;
+    UITextAlignment _alignment;
+    NSString *_value2;
+}
+
+@property UITextAlignment textAlignment;
+@property (nonatomic, retain) UILabel *mainLabel;
+@property (nonatomic, retain) UILabel *secondaryLabel;
+
+- (id)initWithValue:(NSString *)val andValueTwo:(NSString *)val2 andMethod:(SEL)method;
+
+@end
