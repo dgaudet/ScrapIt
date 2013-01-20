0ef30f14c05ce9414c5589f8fd0943cbe26cf0bb
dgaudet86 <dgaudet86@gmail.com>
2013-01-20 22:15:08 +0000

    - Modify TwoTableViewRow to have customizable font and colours for each label in the cell, specifically to allow them to work properly in a UITableView so they would be Dequeued properly

--- Classes/Domain/TwoTableViewRow.h
+++ Classes/Domain/TwoTableViewRow.h
@@ -13,8 +13,10 @@
 }
 
 @property UITextAlignment textAlignment;
-@property (nonatomic, retain) UILabel *mainLabel;
-@property (nonatomic, retain) UILabel *secondaryLabel;
+@property (nonatomic, retain) UIFont *label1Font;
+@property (nonatomic, retain) UIColor *label1Color;
+@property (nonatomic, retain) UIFont *label2Font;
+@property (nonatomic, retain) UIColor *label2Color;
 @property UITableViewCellSelectionStyle cellSelectionStyle;
 
 - (id)initWithValue:(NSString *)val andValueTwo:(NSString *)val2 andMethod:(SEL)method;
--- Classes/Domain/TwoTableViewRow.m
+++ Classes/Domain/TwoTableViewRow.m
@@ -20,7 +20,10 @@ NSString * const TTVR_CELL_IDENTIFIER = @"TTVR_CELL_IDENTIFIER";
 
 @implementation TwoTableViewRow
 
-@synthesize mainLabel, secondaryLabel;
+@synthesize label1Font = _label1Font;
+@synthesize label1Color = _label1Color;
+@synthesize label2Font = _label2Font;
+@synthesize label2Color = _label2Color;
 @synthesize textAlignment = _textAlignment;
 @synthesize cellSelectionStyle = _cellSelectionStyle;
 
@@ -30,20 +33,13 @@ NSString * const TTVR_CELL_IDENTIFIER = @"TTVR_CELL_IDENTIFIER";
         _textAlignment = UITextAlignmentLeft;
         _cellSelectionStyle = UITableViewCellSelectionStyleGray;
         _value2 = val2;
+        _label1Font = [UIFont fontWithName:@"Helvetica-Bold" size:17.0];
+        _label1Color = [UIColor blackColor];
+        
+        _label2Font = [UIFont fontWithName:@"Helvetica-Bold" size:14.0];
+        [_label2Font retain];
+        _label2Color = [UIColor darkGrayColor];
     }
-    self.mainLabel = [[UILabel alloc] init];
-    mainLabel.textColor = [UIColor blackColor];
-    mainLabel.backgroundColor = [UIColor clearColor];
-    mainLabel.lineBreakMode = UILineBreakModeWordWrap;
-    mainLabel.numberOfLines = 999;
-    mainLabel.font = [UIFont fontWithName:@"Helvetica-Bold" size:17.0];
-    
-    self.secondaryLabel = [[UILabel alloc] init];
-    secondaryLabel.textColor = [UIColor darkGrayColor];
-    secondaryLabel.backgroundColor = [UIColor clearColor];
-    secondaryLabel.lineBreakMode = UILineBreakModeWordWrap;
-    secondaryLabel.numberOfLines = 999;    
-    secondaryLabel.font = [UIFont fontWithName:@"Helvetica" size:14.0];
     
     return self;
 }
@@ -58,28 +54,44 @@ NSString * const TTVR_CELL_IDENTIFIER = @"TTVR_CELL_IDENTIFIER";
     if (cell == nil) {
         cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:CellIdentifier] autorelease];
         
+        UILabel *mainLabel = [[UILabel alloc] init];
         [mainLabel setFrame:[self groupedInnerContentRect]];
-        mainLabel.textAlignment = _textAlignment;
+        mainLabel.backgroundColor = [UIColor clearColor];
+        mainLabel.lineBreakMode = UILineBreakModeWordWrap;
+        mainLabel.numberOfLines = 999;
         mainLabel.tag = mainLabelTag;
 		[cell.contentView addSubview:mainLabel];
         
-        [secondaryLabel setFrame:[self subLabelContentRect]];
-        secondaryLabel.textAlignment = _textAlignment;
+        UILabel *secondaryLabel = [[UILabel alloc] init];
+        [secondaryLabel setFrame:[self subLabelContentRect]];        
+        secondaryLabel.backgroundColor = [UIColor clearColor];
+        secondaryLabel.lineBreakMode = UILineBreakModeWordWrap;
+        secondaryLabel.numberOfLines = 999;        
         secondaryLabel.tag = subLabelTag;
 		[cell.contentView addSubview:secondaryLabel];
     }
     
-    // Set up the cell...
-    CGRect rect = mainLabel.frame;
-    rect.size.height = [self heightForString:value withWidth:[self groupedInnerContentRect].size.width withFont:mainLabel.font];
-    mainLabel.frame = rect;
+    UILabel *mainLabel = (UILabel *) [cell.contentView viewWithTag:mainLabelTag];
+    CGRect rect = mainLabel.frame;    
+    mainLabel.textAlignment = _textAlignment;
+    mainLabel.font = _label1Font;
+    mainLabel.textColor = _label1Color;
 	mainLabel.text = value;
     
+    rect.size.height = [self heightForString:value withWidth:[self groupedInnerContentRect].size.width withFont:mainLabel.font];
+    mainLabel.frame = rect;
+    
+    UILabel *secondaryLabel = (UILabel *) [cell.contentView viewWithTag:subLabelTag];
     CGRect subLabelRect = secondaryLabel.frame;
     subLabelRect.origin.y = mainLabel.frame.size.height;
-    subLabelRect.size.height = [self heightForString:_value2 withWidth:[self subLabelContentRect].size.width withFont:secondaryLabel.font];
+    secondaryLabel.font = _label2Font;
+    secondaryLabel.textAlignment = _textAlignment;
+    secondaryLabel.textColor = _label2Color;
+    secondaryLabel.text = _value2;
+    
+    subLabelRect.size.height = [self heightForString:_value2 withWidth:[self subLabelContentRect].size.width withFont:secondaryLabel.font];    
     secondaryLabel.frame = subLabelRect;
-	secondaryLabel.text = _value2;
+    
     
     [cell setSelectionStyle:_cellSelectionStyle];
     
@@ -88,12 +100,13 @@ NSString * const TTVR_CELL_IDENTIFIER = @"TTVR_CELL_IDENTIFIER";
 
 - (CGFloat)heightForString:(NSString *)string withWidth:(CGFloat)width withFont:(UIFont *)font {
     CGSize size = [string sizeWithFont:font constrainedToSize:CGSizeMake(width, 485.0) lineBreakMode:UILineBreakModeWordWrap];
-    return size.height + 5;
+    CGFloat height = size.height + 5;
+    return height;
 }
 
 - (CGFloat)heightForRow {
-    CGFloat mainLabelHeight = [self heightForString:value withWidth:[self groupedInnerContentRect].size.width withFont:mainLabel.font];
-    CGFloat secondaryLabelHeight = [self heightForString:_value2 withWidth:[self groupedInnerContentRect].size.width withFont:secondaryLabel.font];
+    CGFloat mainLabelHeight = [self heightForString:value withWidth:[self groupedInnerContentRect].size.width withFont:_label1Font];
+    CGFloat secondaryLabelHeight = [self heightForString:_value2 withWidth:[self groupedInnerContentRect].size.width withFont:_label2Font];
     return mainLabelHeight + secondaryLabelHeight;
 }
 
@@ -106,8 +119,10 @@ NSString * const TTVR_CELL_IDENTIFIER = @"TTVR_CELL_IDENTIFIER";
 }
 
 - (void)dealloc {
-    [self.mainLabel release];
-    [self.secondaryLabel release];
+    [_label1Color release];
+    [_label1Font release];
+    [_label2Color release];
+    [_label2Font release];
     [super dealloc];
 }
 
