e886d0cac033d5aa3e09d869f769c833f29e0754
dgaudet86 <dgaudet86@gmail.com>
2013-02-18 16:10:47 +0000

    - UserRepository is now properly saving and retrieving the user's province from the user defaults

--- Classes/Repositories/UserRepository.m
+++ Classes/Repositories/UserRepository.m
@@ -7,6 +7,11 @@
 //
 
 #import "UserRepository.h"
+#import "Province.h"
+#import "User.h"
+#import "ProvinceService.h"
+
+NSString *const UR_User_Province_Code = @"Province_Code";
 
 @implementation UserRepository
 
@@ -22,12 +27,28 @@
     return master;
 }
 
+- (id)init {
+    self = [super init];
+    if (self) {
+        _userDefaults = [NSUserDefaults standardUserDefaults];
+        _provinceService = [ProvinceService sharedInstance];
+    }
+    
+    return self;
+}
+
 - (User *)retrieveUser {
+    NSString *provinceCode = [_userDefaults stringForKey:UR_User_Province_Code];
+    if (provinceCode) {
+        Province *province = [_provinceService retrieveProvinceWithCode:provinceCode];
+        User *user = [[User alloc] initWithProvince:province];
+        return [user autorelease];
+    }
     return nil;
 }
 
 - (void)saveUser:(User *)user {
-    
+    [_userDefaults setObject:user.province.code forKey:UR_User_Province_Code];
 }
 
 @end
