21bf6f28843d3334a4ef4bb1fdb3bda5e27f79ba
dgaudet86@gmail.com <dgaudet86@gmail.com@9b412eb8-cd5c-11de-b49f-713b138df6af>
2013-03-01 04:45:09 +0000

    - Move background view setting into ThemeHelper

---
+++ ScrapIt/trunk/Classes/Utilities/ThemeHelper.h
@@ -0,0 +1,15 @@
+//
+//  ThemeHelper.h
+//  ScrapIt
+//
+//  Created by Dean on 2013-02-28.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ThemeHelper : NSObject
+
++ (void)setBackgroundViewForTableView:(UITableView *)tableView;
+
+@end
---
+++ ScrapIt/trunk/Classes/Utilities/ThemeHelper.m
@@ -0,0 +1,35 @@
+//
+//  ThemeHelper.m
+//  ScrapIt
+//
+//  Created by Dean on 2013-02-28.
+//
+//
+
+#import "ThemeHelper.h"
+#import "DeviceUtil.h"
+
+@implementation ThemeHelper
+
+//+ (void)setBackgroundViewForTableView:(UITableView *)tableView {
+//    NSString *imageName = @"PaperTexture";
+//    if ([DeviceUtil isCurrentDeviceIPhone5]) {
+//        imageName = @"PaperTexture-568h";
+//    }
+//    tableView.backgroundColor = [UIColor clearColor];
+//    tableView.backgroundView = nil;
+//    CGRect backgroundRect = [[UIScreen mainScreen] bounds];
+//    UIImageView *backgroundView = [[UIImageView alloc] initWithFrame:backgroundRect];
+//    
+//    [backgroundView setImage:[UIImage imageNamed:imageName]];
+//    [backgroundView setContentMode:UIViewContentModeBottom];
+//    
+//    [tableView setBackgroundView:backgroundView];
+//}
+
++ (void)setBackgroundViewForTableView:(UITableView *)tableView {
+    tableView.backgroundColor = [UIColor clearColor];
+    tableView.backgroundView = nil;
+}
+
+@end
