543445df1029a8df08b3bda2444b584f746837d7
dgaudet86@gmail.com <dgaudet86@gmail.com@9b412eb8-cd5c-11de-b49f-713b138df6af>
2013-03-02 00:06:16 +0000

    - Using code to get the proper background view for iOS 5.1, and iOS 6.0 for non retina, retina and iPhone 5's

--- ScrapIt/trunk/Classes/Utilities/ThemeHelper.m
+++ ScrapIt/trunk/Classes/Utilities/ThemeHelper.m
@@ -11,25 +11,25 @@
 
 @implementation ThemeHelper
 
-//+ (void)setBackgroundViewForTableView:(UITableView *)tableView {
-//    NSString *imageName = @"PaperTexture";
-//    if ([DeviceUtil isCurrentDeviceIPhone5]) {
-//        imageName = @"PaperTexture-568h";
-//    }
-//    tableView.backgroundColor = [UIColor clearColor];
-//    tableView.backgroundView = nil;
-//    CGRect backgroundRect = [[UIScreen mainScreen] bounds];
-//    UIImageView *backgroundView = [[UIImageView alloc] initWithFrame:backgroundRect];
-//    
-//    [backgroundView setImage:[UIImage imageNamed:imageName]];
-//    [backgroundView setContentMode:UIViewContentModeBottom];
-//    
-//    [tableView setBackgroundView:backgroundView];
-//}
-
 + (void)setBackgroundViewForTableView:(UITableView *)tableView {
+    NSString *imageName = @"PaperTexture";
+    if ([DeviceUtil isCurrentDeviceIPhone5]) {
+        imageName = @"PaperTexture-568h";
+    }
     tableView.backgroundColor = [UIColor clearColor];
     tableView.backgroundView = nil;
+    CGRect backgroundRect = [[UIScreen mainScreen] bounds];
+    UIImageView *backgroundView = [[UIImageView alloc] initWithFrame:backgroundRect];
+    
+    [backgroundView setImage:[UIImage imageNamed:imageName]];
+    [backgroundView setContentMode:UIViewContentModeBottom];
+    
+    [tableView setBackgroundView:backgroundView];
 }
+
+//+ (void)setBackgroundViewForTableView:(UITableView *)tableView {
+//    tableView.backgroundColor = [UIColor clearColor];
+//    tableView.backgroundView = nil;
+//}
 
 @end
