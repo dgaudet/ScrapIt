abe302a07fd7a689bc573f424a122a0d66789237
dgaudet86 <dgaudet86@gmail.com>
2013-03-08 20:01:46 +0000

    - removing some unused constants

--- Constants.h
+++ Constants.h
@@ -9,11 +9,8 @@
 #ifndef ScrapIt_Constants_h
 #define ScrapIt_Constants_h
 
-#define kYellowPagesApiKey @"9k5g4bqucenr9ztnh9x693cw"
 #define kYellowPagesBadgeLinkUrl @"http://badge.yellowapi.com/"
 #define kYellowPagesLegalText @"Yellow Pages Group & Design is a trademark of Yellow Pages Group Co. in Canada."
-
-#define kYellowPagesBaseUrl @"http://api.sandbox.yellowapi.com"
 
 #define kSystemDefaultFont [UIFont fontWithName:@"Helvetica-Bold" size:17.0]
 
