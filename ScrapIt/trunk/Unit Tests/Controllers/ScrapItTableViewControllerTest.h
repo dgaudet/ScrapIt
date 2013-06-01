6f4653fd938205a2be02c2d59a0fd08154aeab7f
dgaudet86@gmail.com <dgaudet86@gmail.com@9b412eb8-cd5c-11de-b49f-713b138df6af>
2013-06-01 02:56:52 +0000

    - Adding Mocking framework for unit testing, adding unit test for controller, however it's not currently working

---
+++ ScrapIt/trunk/Unit Tests/Controllers/ScrapItTableViewControllerTest.h
@@ -0,0 +1,18 @@
+//
+//  ScrapItTableViewControllerTest.h
+//  ScrapIt
+//
+//  Created by Dean on 2013-05-31.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+//@class ScrapItTableViewController;
+@class MapViewController;
+
+@interface ScrapItTableViewControllerTest : SenTestCase {
+//    ScrapItTableViewController *_controller;
+    MapViewController *_controller;
+}
+
+@end
