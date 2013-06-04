a76b40f5971f38708aaf750e8056b22e91864282
dgaudet86@gmail.com <dgaudet86@gmail.com@9b412eb8-cd5c-11de-b49f-713b138df6af>
2013-06-04 04:22:12 +0000

    - adding ability to check if the current device is equal to a specific version - display different messages as to where the user can find location settings - adding macro's for device versions to constants

--- ScrapIt/trunk/Constants.h
+++ ScrapIt/trunk/Constants.h
@@ -9,6 +9,12 @@
 #ifndef ScrapIt_Constants_h
 #define ScrapIt_Constants_h
 
+#define SYSTEM_VERSION_EQUAL_TO(v)                  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedSame)
+#define SYSTEM_VERSION_GREATER_THAN(v)              ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedDescending)
+#define SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
+#define SYSTEM_VERSION_LESS_THAN(v)                 ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
+#define SYSTEM_VERSION_LESS_THAN_OR_EQUAL_TO(v)     ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedDescending)
+
 #define kYellowPagesBadgeLinkUrl @"http://badge.yellowapi.com/"
 #define kYellowPagesLegalText @"Yellow Pages Group & Design is a trademark of Yellow Pages Group Co. in Canada."
 
